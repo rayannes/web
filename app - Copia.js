6f950078fcc9396e2a7c503d6613c20ec17818fe
rayannesilva <56279350+rayannesilva@users.noreply.github.com>
2020-09-27 09:40:03 -0300

    Add files via upload

---
+++ app - Copia.js
@@ -0,0 +1,18 @@
+
+const express = require ('express')
+const app = express()
+const cidade = { "Crato":"total de habitantes: 29"  ,"Juazeiro":"total de habitantes: 39" ,"Barbalha":"total de habitantes: 1000000"}
+
+
+app.get('/cidade',(req, res)=> {//questao 1
+	res.json(cidade)
+})
+app.get('/cadastro',(req, res) =>{//questao 2
+	
+	res.type('html')
+	res.end('<form><textarea name="texto" rows="10" cols="30"></textarea> <button>botão</button></form>')
+})
+
+app.listen(3000, () =>{//testando
+	console.log('funcionando')
+})
